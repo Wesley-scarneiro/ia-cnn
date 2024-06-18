af2827ba0b73712739c0d6ede500b66dcb64238f
Wesley <Wesley1995.silva@gmail.com>
2024-06-17 23:55:05 -0300

    keras

--- data_functions.py
+++ data_functions.py
@@ -10,6 +10,5 @@ def open_file(file_path):
 def create_data_cnn(labels_path, dataset_path) -> list[DataCnn]:
     labels = open_file(labels_path)
     images_name = sorted(os.listdir(dataset_path), key=lambda x: int(x.split('.')[0]))
-    data_cnn = [DataCnn(image, label) for image, label in zip(images_name, labels[:len(images_name)])]
+    data_cnn = [DataCnn(image, label) for image, label in zip(images_name, labels)]
     return data_cnn
-
--- main.py
+++ main.py
@@ -1,59 +1,20 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import SGD
 
-# Configurações iniciais da CNN
-def initialize_cnn():
-    classifier = Sequential()
-    classifier.add(Input(shape=(10, 12, 1)))  # Adiciona explicitamente uma camada de entrada
+model = Sequential()
 
-    # Primeira camada
-    classifier.add(Conv2D(32, (3, 3), activation='relu'))
-    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+# Camadas convolucionais
+model.add(Conv2D(8, (3, 3), activation='relu', input_shape=(10, 12, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
 
-    # Segunda camada
-    classifier.add(Conv2D(32, (3, 3), activation='relu'))
-    classifier.add(MaxPooling2D(pool_size=(2, 2)))
-    classifier.add(Flatten())
+# Camadas densas
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='softmax'))
 
-    # Fully connected layers
-    classifier.add(Dense(units=128, activation='relu'))
-    classifier.add(Dense(units=26, activation='softmax'))  # 26 unidades para 26 classes
-
-    # Compilação do modelo
-    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    
-    return classifier
-
-def images_train_validation():
-    train_datagen = ImageDataGenerator(rescale=1./255,
-                                       shear_range=0.2,
-                                       zoom_range=0.2,
-                                       horizontal_flip=True)
-    validation_datagen = ImageDataGenerator(rescale=1./255)
-
-    # Pré-processamento das imagens de treino e validação
-    training_set = train_datagen.flow_from_directory('data/dataset_train',
-                                                     target_size=(10, 12), 
-                                                     batch_size=32,
-                                                     class_mode='categorical')
-
-    validation_set = validation_datagen.flow_from_directory('data/dataset_validation',
-                                                            target_size=(10, 12), 
-                                                            batch_size=32,
-                                                            class_mode='categorical')
-    return training_set, validation_set
-
-# classifier = initialize_cnn()
-# training_set, validation_set = images_train_validation()
-
-# # Executando o treinamento
-# classifier.fit(training_set,
-#                steps_per_epoch=100,
-#                epochs=5,
-#                validation_data=validation_set,
-#                validation_steps=100)
-
-
-
+sgd = SGD(learning_rate=0.1)
+model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
